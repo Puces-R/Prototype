4cc63b1da73e628c5676d0e67b264c3997ba7aa9
Puces-R <lp.page@cgodin.qc.ca>
2014-01-21 06:48:21 -0500

    Script de création des tables initiales

---
+++ Scripts/000-CreationTablesInitiales.sql
@@ -0,0 +1,14 @@
+SELECT * INTO BD6B8_424R.dbo.PPArticlesEnPanier FROM BD6B8PetitesPucesH2014Effective.dbo.PPArticlesEnPanier
+SELECT * INTO BD6B8_424R.dbo.PPCategories FROM BD6B8PetitesPucesH2014Effective.dbo.PPCategories
+SELECT * INTO BD6B8_424R.dbo.PPClients FROM BD6B8PetitesPucesH2014Effective.dbo.PPClients
+SELECT * INTO BD6B8_424R.dbo.PPCommandes FROM BD6B8PetitesPucesH2014Effective.dbo.PPCommandes
+SELECT * INTO BD6B8_424R.dbo.PPDetailsCommandes FROM BD6B8PetitesPucesH2014Effective.dbo.PPDetailsCommandes
+SELECT * INTO BD6B8_424R.dbo.PPHistoriquePaiements FROM BD6B8PetitesPucesH2014Effective.dbo.PPHistoriquePaiements
+SELECT * INTO BD6B8_424R.dbo.PPPoidsLivraisons FROM BD6B8PetitesPucesH2014Effective.dbo.PPPoidsLivraisons
+SELECT * INTO BD6B8_424R.dbo.PPProduits FROM BD6B8PetitesPucesH2014Effective.dbo.PPProduits
+SELECT * INTO BD6B8_424R.dbo.PPTaxeFederale FROM BD6B8PetitesPucesH2014Effective.dbo.PPTaxeFederale
+SELECT * INTO BD6B8_424R.dbo.PPTaxeProvinciale FROM BD6B8PetitesPucesH2014Effective.dbo.PPTaxeProvinciale
+SELECT * INTO BD6B8_424R.dbo.PPTypesLivraison FROM BD6B8PetitesPucesH2014Effective.dbo.PPTypesLivraison
+SELECT * INTO BD6B8_424R.dbo.PPTypesPoids FROM BD6B8PetitesPucesH2014Effective.dbo.PPTypesPoids
+SELECT * INTO BD6B8_424R.dbo.PPVendeurs FROM BD6B8PetitesPucesH2014Effective.dbo.PPVendeurs
+SELECT * INTO BD6B8_424R.dbo.PPVendeursClients FROM BD6B8PetitesPucesH2014Effective.dbo.PPVendeursClients

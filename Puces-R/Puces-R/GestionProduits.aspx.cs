9e70f69ca62df8da87ba2b7c5a24a8e4278b3fda
Louis-Pierre <lp.page@cgodin.qc.ca>
2014-02-12 10:15:39 -0500

    Nom variables pas correctes

--- Puces-R/Puces-R/GestionProduits.aspx.cs
+++ Puces-R/Puces-R/GestionProduits.aspx.cs
@@ -158,11 +158,11 @@ namespace Puces_R
                     Button btnSupprimer = (Button)item.FindControl("btnSupprimer");
                     Button btnModifier = (Button)item.FindControl("btnModifier");
 
-                    DataRowView drvFilm = (DataRowView)e.Item.DataItem;
+                    DataRowView drvProduit = (DataRowView)e.Item.DataItem;
 
-                    long noProduit = (long)drvFilm["NoProduit"];
+                    long noProduit = (long)drvProduit["NoProduit"];
 
-                    Object photo = drvFilm["Photo"];
+                    Object photo = drvProduit["Photo"];
                     String urlImage;
                     if (photo is DBNull)
                     {
@@ -172,10 +172,10 @@ namespace Puces_R
                     {
                         urlImage = "Images/Televerse/" + (String)photo;
                     }
-                    String strCategorie = (String)drvFilm["Description"];
-                    String strDescriptionAbregee = (String)drvFilm["Nom"];
-                    decimal decPrixDemande = (decimal)drvFilm["PrixDemande"];
-                    short intQuantite = (short)drvFilm["NombreItems"];
+                    String strCategorie = (String)drvProduit["Description"];
+                    String strDescriptionAbregee = (String)drvProduit["Nom"];
+                    decimal decPrixDemande = (decimal)drvProduit["PrixDemande"];
+                    short intQuantite = (short)drvProduit["NombreItems"];
 
                     lblNoProduit.Text = "No. " + noProduit.ToString();
                     imgProduit.ImageUrl = urlImage;
@@ -197,7 +197,7 @@ namespace Puces_R
                 //Button btnSupprimer = (Button)item.FindControl("btnSupprimer");
                 //Button btnModifier = (Button)item.FindControl("btnModifier");
 
-                //DataRowView drvFilm = (DataRowView)e.Item.DataItem;
+                // = (DataRowView)e.Item.DataItem;
 
                 //long noProduit = (long)drvFilm["NoProduit"];
                 //String urlImage = "Images/Televerse/" + (String)drvFilm["Photo"];
--- Puces-R/Puces-R/Produits.aspx.designer.cs
+++ Puces-R/Puces-R/Produits.aspx.designer.cs
@@ -121,6 +121,15 @@ namespace Puces_R {
         protected global::System.Web.UI.WebControls.Button btnRechercheAvance;
         
         /// <summary>
+        /// Contrôle mvProduits.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView mvProduits;
+        
+        /// <summary>
         /// Contrôle dtlProduits.
         /// </summary>
         /// <remarks>
